6f191293c68f22610b5ae1b2e01fad9ffe642745
Richard George <RichiRic@users.noreply.github.com>
2023-12-05 20:33:53 +0530

    Update theme-2-script.js

--- Scripts/theme-2-script.js
+++ Scripts/theme-2-script.js
@@ -1,5 +1,3 @@
-<script language="JavaScript" type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 //Check Current OS Theme and Implement Changes
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 let btnlbl = document.getElementById("modebtn-lbl");
